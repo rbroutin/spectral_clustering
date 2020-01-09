225c9c707f5daca48f7f55c3857cf110d358e626
rbroutin <broutin.romain@gmail.com>
2020-01-09 11:49:44 +0100

    fonction nngraph terminée

--- code.R
+++ code.R
@@ -1,4 +1,4 @@
-# Chargement des donnees et tests
+# Chargement des donnees et visualisation
 load("smiley.RData")
 plot(D$x, col=D$classes)
 df <- as.data.frame(D$x)
@@ -8,14 +8,6 @@ dist_euclidienne <- function(x, y) {
   diff <- sum((x-y)**2)
   d <- sqrt(diff)
   return(d)
-}
-
-norme <- function(x){
-  return(sqrt(x%*%x))
-}
-
-gaussian <- function(x, y, sigma){
-  return(exp(-(norme(x-y)**2)/2*sigma**2))
 }
 
 pause <- function(){readline("Press <ENTER> to Continue.")} 
@@ -62,40 +54,103 @@ my_kmeans <- function(X, k, niter = 20) {
 
 my_kmeans(df, 4)
 
-my_nngraph <- function(X, similarity = "linear", neighbor = "seuil") {
+my_nngraph <- function(X, similarity = "linear", neighbor = "seuil", sigma = 1/8, deg = 3, theta = 0, k = 3) {
   # transformer en matrix pour appliquer le produit matriciel
   M_X <- as.matrix(X)
   
+  # choisi la fonction de similarite
+  if(similarity=="linear"){
+    similarity_function <- function(Xi, Xj){
+      return(Xi%*%Xj)
+    }
+  }
+  else if(similarity=="gaussian"){
+    similarity_function <- function(Xi, Xj){
+      return(exp(-((Xi-Xj)%*%(Xi-Xj))/(2*sigma**2)))
+    }
+  }
+  else if(similarity=="polynomial"){
+    similarity_function <- function(Xi, Xj){
+      return((Xi%*%Xj+1)**deg)
+    }
+  }
+  else{stop("Wrong value for <similarity> parameter")}
+  
+  # conditions sur le voisinnage
+  neighbor_theta <- ifelse(neighbor == "seuil" | neighbor == "connexe", TRUE, FALSE)
+  neighbor_k <- ifelse(neighbor == "knn", TRUE, FALSE)
+  if(!neighbor_theta & !neighbor_k){stop("Wrong value for <neighbor> parameter")}
+  
+  # boucle pour creer la matrice de similarite
+  S <- matrix(ncol=nrow(M_X), nrow=nrow(M_X))
+  
+  for(i in 1:nrow(M_X)){
+    for(j in i:nrow(M_X)){
+      Sij <- similarity_function(M_X[i,], M_X[j,])
+      S[i, j] <- ifelse(neighbor_theta, ifelse(Sij>=theta, Sij, 0), Sij) # si seuil ou connexe, on transforme la similarite en voisinnage a la vole
+      S[j, i] <- S[i, j]
+    }
+  }
+  diag(S) <- 0
+  
+  # matrice de voisinnage
+  if(neighbor_theta){W <- S}
+  else{
+    W <- matrix(ncol=nrow(M_X), nrow=nrow(M_X))
+    
+    for(i in 1:nrow(M_X)){
+      for(j in i:nrow(M_X)){
+        condition_knn <- (j%in%which(S[i,] %in% sort(S[i,], decreasing = TRUE)[1:k]) | i%in%which(S[j,] %in% sort(S[j,], decreasing = TRUE)[1:k]))
+        W[i, j] <- ifelse(condition_knn, S[i, j], 0)
+        W[j, i] <- W[i, j]
+      }
+    }
+    
+    diag(W) <- 0
+  }
+  
+  return(W)
+  
 }
+
+my_nngraph(df[1:6,], similarity = "polynomial", neighbor = "knn", deg = 2, k = 4)
+
 
 ## brouillon
 M_X <- as.matrix(df)
 M_X <- M_X[1:6,]
-M_X
+M_X <- as.matrix(my.data)
 S <- matrix(ncol = nrow(M_X), nrow = nrow(M_X))
-sigma = 1/8
+sigma = sqrt(1/2)
 deg = 2
 knn <- 3
-
+M_X
 for(i in 1:nrow(M_X)) {
   for(j in 1:nrow(M_X)) {
     
     # linear
     #if(i>j){S[i, j] <- ifelse(M_X[i,]%*%M_X[j,]>=0,M_X[i,]%*%M_X[j,],0)}
+    #S[j, i] <- S[i, j]
     # gaussian
-    #if(i!=j){S[i, j] <- exp(-norme(M_X[i,]-M_X[j,])**2/(2*sigma**2))}
+    if(i!=j){S[i, j] <- exp(-norme(M_X[i,]-M_X[j,]**2)/(2*sigma**2))}
+    S[j, i] <- S[i, j]
     # polynomial
     #if(i>j){S[i, j] <- (M_X[i,]%*%M_X[j,]+1)**deg
     #S[j, i] <- S[i, j]
     #}
     # gaussian via function
-    if(i!=j){S[i, j] <- gaussian(M_X[i,], M_X[j,], sigma)}
+    #if(i!=j){S[i, j] <- gaussian(M_X[i,], M_X[j,], sigma)}
     #print(which(S[j,] %in% sort(S[j,], decreasing = TRUE)[1:knn]))
   }
 }
 
-diag(S) <- 0
 S
+
+c(1,2)%*%c(1,2)
+norm(as.matrix(c(1,2)), type="F")
+
+diag(S) <- 0
+S[1:8, 1:8]
 
 W <- matrix(nrow=nrow(M_X), ncol = nrow(M_X))
 
@@ -113,5 +168,3 @@ for(i in 1:nrow(M_X)) {
 }
 
 W_1 <- W
-
-
